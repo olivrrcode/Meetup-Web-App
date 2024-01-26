cc45f951b35a994997b9dcd2e4fb372627466762
olivrrcode <o.atherton@outlook.com>
2024-01-26 01:04:50 +0000

    exported all the context

--- src/store/favourites-context.js
+++ src/store/favourites-context.js
@@ -8,7 +8,7 @@ const FavouritesContext = createContext({
   itemIsFavourite: (meetupId) => {},
 });
 
-function FavouritesContextProvider(props) {
+export function FavouritesContextProvider(props) {
   const [userFavourites, setUserFavourites] = useState([]);
 
   function addFavouriteHandler(favouriteMeetup) {
@@ -41,3 +41,5 @@ function FavouritesContextProvider(props) {
     </FavouritesContext.Provider>
   );
 }
+
+export default FavouritesContext;
