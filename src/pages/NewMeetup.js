e3a0c143d9ba9b1967e540fad34363132f949b95
olivrrcode <o.atherton@outlook.com>
2024-01-26 00:17:20 +0000

    Added POST request to firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
+  function addMeetupHandler(meetupdata) {
+    fetch(
+      "https://react-getting-started-3e96b-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupdata),
+        header: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
